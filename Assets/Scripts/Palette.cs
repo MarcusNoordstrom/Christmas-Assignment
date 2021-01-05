557d97dbcf6c0cb4dd4e934501f8f56dee4f56ae
Bulteze <48334398+Bulteze@users.noreply.github.com>
2021-01-05 14:03:40 +0100

    Update
    Trying to make so all tiles load from folder at start.

--- Assets/Scripts/Palette.cs
+++ Assets/Scripts/Palette.cs
@@ -1,19 +1,25 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 public class Palette : MonoBehaviour {
     Tile _tile => GetComponent<Tile>();
     public static List<GameObject> TilePalette;
+    public GameObject[] tiles;
     
      void Awake()
     {
-        TilePalette = new List<GameObject>(); 
-        AddToPalette(Color.green, "Grass");
-        AddToPalette(Color.blue, "Water");
+        TilePalette = new List<GameObject>();
+        tiles = Resources.LoadAll("Tiles").Cast<GameObject>().ToArray();
+
+        foreach (var tile in tiles) {
+            AddToPalette(Color.black, "TEST");
+        }
     }
 
-    public void AddToPalette(Color color, string tileName)
+     public void AddToPalette(Color color, string tileName)
     {
         TilePalette.Add(_tile.CreateTile(color, tileName));
     }
